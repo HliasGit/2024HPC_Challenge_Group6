20f1339d332c48276c3fc91c4439f613f5fd5b61
nicola-de-march <nicola.demarchmail.polimi.it>
2024-02-27 22:43:25 +0100

    Test

--- conjugate_gradients-main/modulesBeforeCompiling.sh
+++
@@ -1,6 +0,0 @@
-# Execute with source instead of ./
-
-module load intel
-# module load OpenBLAS/0.3.23-GCC-12.3.0
-# module load FlexiBLAS/3.3.1-GCC-12.3.0
-#cblas_dgemv(CblasRowMajor, CblasNoTrans, size, size, 1.0, A, size, p, 1, 0.0, Ap, 1);
