ec5d8415887668a1556a1b1ff3e361b2f9d75d95
nicola-de-march <nicola.demarchmail.polimi.it>
2024-02-29 19:13:54 +0100

    Some tests

---
+++ conjugate_gradients-main/src/test.cu
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cuda_runtime.h>
+#include "cublas_v2.h"
+#define M 6
+#define N 5
+#define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1))
+
+int main (void)
+{
+    cudaError_t cudaStat;
+    cublasStatus_t stat;
+    cublasHandle_t handle;
+
+    int i, j;
+    float* devPtrA;
+    float* a = 0;
+    a = (float *)malloc (M * N * sizeof (*a));
+
+    if (!a) {
+        printf ("host memory allocation failed");
+        return EXIT_FAILURE;
+    }
+    for (j = 1; j <= N; j++) {
+        for (i = 1; i <= M; i++) {
+            a[IDX2F(i,j,M)] = (float)((i-1) * N + j);
+        }
+    }
+    cudaStat = cudaMalloc ((void**)&devPtrA, M*N*sizeof(*a));
+    printf("%s", cudaGetErrorString(cudaStat));
+
+    // if (cudaStat != cudaSuccess) {
+    //     printf ("Memory allcation failed");
+    //     printf ("Error value = %s ", cudaStat);
+
+    //     return EXIT_FAILURE;
+    // }
+    return 0;
+}
